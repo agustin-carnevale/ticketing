390fcfe155777db2d4e6ca51674bc4aa1dd7afbc
Agustin Carnevale <agustincarnevale@Agustins-MacBook-Pro.local>
2020-08-06 02:05:28 -0300

    updated startup messages

--- client/api/build_client.js
+++ client/api/build_client.js
@@ -4,7 +4,7 @@ const buildClient = ({req}) => {
   if (typeof window === 'undefined'){
     //we are on the server
     return axios.create({
-      baseURL: 'http://www.ticketing-app-prod.cyou',
+      baseURL: 'http://www.ticketing-app-prod.cyou/',
       headers: req.headers
     })
   }else{
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,7 +5,7 @@ import {OrderCreatedListener} from './events/listeners/order_created_listener'
 import {OrderCancelledListener} from './events/listeners/order_cancelled_listener'
 
 const start = async () => {
-  console.log("Starting up...!!")
+  console.log("Starting up..")
   
   if (!process.env.JWT_KEY){
     throw new Error('JWT_KEY must be defined')
