02976962440bae048f2e7973c53c3c3c0075672a
Agustin Carnevale <agustincarnevale@Agustins-MacBook-Pro.local>
2020-08-05 21:52:06 -0300

    added startup message to orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,6 +7,8 @@ import {ExpirationCompleteListener} from './events/listeners/expiration_complete
 import {PaymentCreatedListener} from './events/listeners/payment_created_listener'
 
 const start = async () => {
+  console.log("Starting...")
+  
   if (!process.env.JWT_KEY){
     throw new Error('JWT_KEY must be defined')
   }
