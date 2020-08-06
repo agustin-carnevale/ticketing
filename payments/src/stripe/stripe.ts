a677718766012c5c659bde15b884c78bdb927249
Agustin Carnevale <agustincarnevale@Agustins-MacBook-Pro.local>
2020-08-05 21:57:28 -0300

    removed some extra tests at deployment in payments service

--- payments/src/stripe/stripe.ts
+++ payments/src/stripe/stripe.ts
@@ -1,10 +1,10 @@
 import Stripe from 'stripe'
 
-if( process.env.NODE_ENV === 'test'){
-  const { STRIPE_SK } = require('../../keys')
-  process.env.STRIPE_SECRET_KEY = STRIPE_SK
-  console.log("TEST_MODE_STRIPE_KEY")
-}
+// if( process.env.NODE_ENV === 'test'){
+//   const { STRIPE_SK } = require('../../keys')
+//   process.env.STRIPE_SECRET_KEY = STRIPE_SK
+//   console.log("TEST_MODE_STRIPE_KEY")
+// }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!,{
   apiVersion: '2020-03-02'
